d1fd3b0d0a752c653abc081dc98a332fe7cee403
Clark Lawrence Ching <Rencechichi@gmail.com>
2025-08-16 20:25:09 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,22 @@
 import discord
 import random
 
+# This is a simple Discord bot that sends a random meme image when a user types !meme in the chat.
 MEME_URLS = [
-    "https://media.discordapp.net/attachments/825010473954574369/1405918383484174479/images3.jpg?ex=68a09307&is=689f4187&hm=6fe4d3ae55f98f643cda5a091b9fabc29b4550c5b154c98456de2fab865c3e2f&=&format=webp",
-    "https://media.discordapp.net/attachments/825010473954574369/1405918383748157672/images4.jpg?ex=68a09307&is=689f4187&hm=b21a71f5b347a30a4e869230255d3e57c2606a2ec720550701de58ee23269604&=&format=webp",
-    "https://media.discordapp.net/attachments/825010473954574369/1405918383958134855/images.jpg?ex=68a09307&is=689f4187&hm=77c61c5adc6a67d6b4399ae60fd1d4839aa011a3ace7c5aa5cca5e31c1a2cd29&=&format=webp",
-    "https://media.discordapp.net/attachments/825010473954574369/1405918384176234506/images_1.jpg?ex=68a09307&is=689f4187&hm=ae318eded24a1b49fb66933d5d655fb710ae50c82614bdd20c03b0ed0d097dbd&=&format=webp",
-    "https://media.discordapp.net/attachments/825010473954574369/1405918384402468924/images_2.jpg?ex=68a09307&is=689f4187&hm=5908606dab79bb51b3cde1b2af2a767ffb8cc0f4b3e7bfce30fca124c1172c75&=&format=webp"
+   "https://i.imgur.com/abc123.jpg",  # Example URL, replace with real image links
+    "https://i.imgur.com/def456.jpg",
+    "https://i.imgur.com/ghi789.jpg",
+    "https://i.imgur.com/jkl012.jpg",
+    "https://i.imgur.com/mno345.jpg"
 ]
+
+# Function to get a random meme URL
 def get_meme():
     return random.choice(MEME_URLS)
-
+  
 # Define the Discord client
 class MyClient(discord.Client):
+  # This is the client that will handle events
   async def on_ready(self):
     print('Logged on as {0}!'.format(self.user))
     # This function is called when the bot is ready and logged in.
@@ -29,5 +33,4 @@ client = MyClient(intents=intents)
 client.run('TOKEN CODE') 
 
 # The token in this example is a placeholder and should not be used.
-
 # Make sure to download the discord.py library and replace 'TOKEN CODE' with your actual bot token.
